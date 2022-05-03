c71a4d6a95a118aae55ef717b8e8397497c2579e
Saahil Patel <saahilp18@gmail.com>
2022-05-03 01:35:04 -0400

    Added buttons to mobile view

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -2,6 +2,8 @@ import "./intro.scss"
 import {KeyboardArrowDown} from "@material-ui/icons"
 import { init } from 'ityped'
 import { useEffect, useRef } from "react"
+import {Person,Mail,LinkedIn, GitHub,Attachment} from "@material-ui/icons"
+
 export default function Intro() {
     
     const textRef = useRef();
@@ -26,7 +28,28 @@ export default function Intro() {
                     <h2>Hey there, I'm</h2>
                     <h1>Saahil Patel</h1>
                     <h3 ><span ref = {textRef}></span></h3>
-
+                    <div className="iconContainer">
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                        <span ><a  href="tel:7328232978" class="contact-button no-print" itemprop="email"> </a></span>
+                    </div> 
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span ><a  href="mailto:saahilp18@gmail.com" class="contact-button no-print" target="_blank" rel="noreferrer" itemprop="email"> </a></span>
+                    </div>
+                    <div className="itemContainer">
+                        <LinkedIn className="icon"/>
+                        <span ><a  href="https://linkedin.com/in/patelsaahil" target="_blank" rel="noreferrer" class="contact-button no-print" itemprop="email"> </a></span>
+                    </div>
+                    <div className="itemContainer">
+                        <GitHub className="icon"/>
+                        <span ><a  href="https://github.com/saahilp18" class="contact-button no-print" target="_blank" rel="noreferrer" itemprop="email"> </a></span>
+                    </div>
+                    <div className="itemContainer">
+                        <Attachment className="icon"/>
+                        <span ><a  href="assets/Resume.pdf" class="contact-button no-print" target="_blank" rel="noreferrer" itemprop="email"> </a></span>
+                    </div>
+                    </div>
                 </div>
                 <a href="#portfolio">
                     <KeyboardArrowDown className="arrowDown"/>
