69d928c308f1f25b1343560c9f307ac207c632a5
Saahil Patel <saahilp18@gmail.com>
2022-05-22 21:38:26 -0400

    spelling fix

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -15,7 +15,7 @@ export default function Portfolio(){
             <br></br>
             <h2 className='company'>United Parcel Service (UPS)</h2>
             <h2 className='location'>Mahwah, NJ • June 2021 - May 2022</h2>
-            <h2 className='companyInfo'>I utilized RSA Encryption to encrypt 70 applications and their clear text passwords and connection strings. I also audited 7 of the UPS load balancers, fixing and cleaning entries that needed to be have their listening ports changed or shut down. Among the plethora of tasks I’ve completed at UPS I also used Grafana Dashboards and Prometheus to analyze the CPU, memory, and storage usage of 180 servers across 12 web farms.</h2>
+            <h2 className='companyInfo'>I utilized RSA Encryption to encrypt 70 applications and their clear text passwords and connection strings. I also audited 7 of the UPS load balancers, fixing and cleaning entries that needed to have their listening ports changed or shut down. Among the plethora of tasks I've completed at UPS I also used Grafana Dashboards and Prometheus to analyze the CPU, memory, and storage usage of 180 servers across 12 web farms.</h2>
             <br></br>
             <br></br>
             <h2 className='company'>Learning Assistant Program</h2>
