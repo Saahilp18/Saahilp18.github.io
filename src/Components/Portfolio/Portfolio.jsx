bded6b0b7ce043c154dc14a4b26b5fe4054dff27
Saahil Patel <saahilp18@gmail.com>
2022-06-19 20:15:24 -0400

    updated ups desc

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -15,7 +15,12 @@ export default function Portfolio(){
             <br></br>
             <h2 className='company'>United Parcel Service (UPS)</h2>
             <h2 className='location'>Mahwah, NJ • June 2021 - May 2022</h2>
-            <h2 className='companyInfo'>I utilized RSA Encryption to encrypt 70 applications and their clear text passwords and connection strings. I also audited 7 of the UPS load balancers, fixing and cleaning entries that needed to have their listening ports changed or shut down. Among the plethora of tasks I've completed at UPS I also used Grafana Dashboards and Prometheus to analyze the CPU, memory, and storage usage of 180 servers across 12 web farms.</h2>
+            <h2 className='companyInfo'>At UPS, I utilized RSA Encryption to scale the encryption of 70 applications and their clear text passwords to
+             reduce security vulnerabilities. I also audited the UPS load balancers, fixing and cleaning entries that needed to have their listening 
+             ports changed or shut down in order for the servers to be recycled and reused. 
+             Among the plethora of tasks I've completed at UPS I also used Grafana Dashboards and Prometheus to analyze the CPU, 
+             memory, and storage usage of 180 servers across 12 web farms which has prevented server outages during times of 
+             peak performance.</h2>
             <br></br>
             <br></br>
             <h2 className='company'>Learning Assistant Program - Data Structures</h2>
